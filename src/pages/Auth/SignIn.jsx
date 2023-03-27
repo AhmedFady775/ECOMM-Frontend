c81ca360d429472676124a600074bd2110eb0d8e
AhmedFady775 <fadiahmed298@gmail.com>
2023-03-27 03:18:06 +0200

    Sign in and up UI updates

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -23,7 +23,7 @@ export default function SignIn() {
   const navigate = useNavigate();
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get("redirect");
-  const redirect = redirectInUrl ? redirectInUrl : "/shop";
+  const redirect = redirectInUrl ? redirectInUrl : "/";
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -48,7 +48,7 @@ export default function SignIn() {
       ctxDispatch({ type: "USER_SIGNIN", payload: data });
       localStorage.setItem("userInfo", JSON.stringify(data));
       dispatch({ type: "LOGIN_SUCCESS" });
-      navigate(redirect || "/shop");
+      navigate(redirect || "/");
       toast.success("Logged in");
     } catch (error) {
       dispatch({ type: "LOGIN_FAIL" });
@@ -63,14 +63,20 @@ export default function SignIn() {
   }, [navigate, redirect, userInfo]);
 
   return (
-    <div className="flex h-screen items-center justify-center bg-white">
-      <div className="flex flex-row items-center md:space-x-32 w-full justify-center">
-        <div className="md:flex hidden flex-col items-center">
-          <img src={mobileLogin} className="w-[500px]" />
+    <div className="flex flex-col">
+      <div className="flex h-[72px] items-center justify-center shadow">
+        <Link to="/">
+          <p className="text-2xl md:text-3xl font-bold Robot">V2S</p>
+        </Link>
+      </div>
+      <div className="flex flex-col items-center lg:w-[378px] lg:max-w-[378px] px-4 lg:mx-auto">
+        <div className="flex my-4 items-center">
+          <img src={mobileLogin} className="w-[200px]" />
         </div>
-
-        <form onSubmit={submitHandler} className="w-[80%] md:w-[30%]">
-          <p className="text-5xl font-bold text-gray-400 mb-10">SIGN IN</p>
+        <p className="text-[#0e001a] mb-2 text-[1.3rem] font-semibold leading-[1.8rem] ">
+          Welcome back!
+        </p>
+        <form onSubmit={submitHandler} className="flex flex-col w-full">
           <div className="flex flex-col mb-4">
             <label className="inputlabel">E-mail</label>
             <div className="inputCont">
@@ -97,21 +103,21 @@ export default function SignIn() {
           <div className="flex flex-col ">
             {loading ? (
               <button
-                className="bg-slate-300 text-white py-2 px-6 rounded flex justify-center items-center "
+                className="bg-slate-300 text-white  p-4 rounded flex justify-center items-center "
                 type="submit"
               >
                 <CircularProgress size={25} thickness={4} color="inherit" />
               </button>
             ) : (
               <button
-                className="bg-teal-500 text-white py-2 px-6 rounded"
+                className="bg-[#0e001a] text-white p-4 rounded-lg hover:bg-[#0e001ab7] transition"
                 type="submit"
               >
                 Sign In
               </button>
             )}
           </div>
-          <div className="flex mt-2">
+          <div className="flex mt-4 text-sm font-normal leading-4 tracking-[0]">
             New customer?{" "}
             <Link
               className="text-sky-500 ml-2"
@@ -121,6 +127,9 @@ export default function SignIn() {
             </Link>
           </div>
         </form>
+        <p className="text-xs font-normal leading-4 tracking-[0] text-center w-[300px] text-[#7f8286] mt-6">
+          By continuing, you agree to our Privacy Policy and Terms of Use.
+        </p>
       </div>
     </div>
   );
--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -25,13 +25,12 @@ export default function SignUp() {
   const navigate = useNavigate();
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get("redirect");
-  const redirect = redirectInUrl ? redirectInUrl : "/shop";
+  const redirect = redirectInUrl ? redirectInUrl : "/";
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordShown, setPasswordShown] = useState(false);
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
@@ -42,10 +41,6 @@ export default function SignUp() {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    if (password !== confirmPassword) {
-      toast.error("Passwords do not match");
-      return;
-    }
     try {
       dispatch({ type: "LOGIN_REQUEST" });
 
@@ -61,7 +56,7 @@ export default function SignUp() {
       ctxDispatch({ type: "USER_SIGNIN", payload: data });
       localStorage.setItem("userInfo", JSON.stringify(data));
       dispatch({ type: "LOGIN_SUCCESS" });
-      navigate(redirect || "/shop");
+      navigate(redirect || "/");
       toast.success("Logged in");
     } catch (error) {
       dispatch({ type: "LOGIN_FAIL" });
@@ -76,29 +71,42 @@ export default function SignUp() {
   }, [navigate, redirect, userInfo]);
 
   return (
-    <div className="flex h-screen items-center justify-center bg-white">
-      <div className="flex flex-row items-center space-x-32 w-full justify-center">
-        <form onSubmit={submitHandler} className="w-[80%] md:w-[30%]">
-          <p className="text-5xl font-bold text-gray-400 mb-10">SIGN UP</p>
+    <div className="flex flex-col">
+      <div className="flex h-[72px] items-center justify-center shadow">
+        <Link to="/">
+          <p className="text-2xl md:text-3xl font-bold Robot">V2S</p>
+        </Link>
+      </div>
+      <div className="flex flex-col items-center lg:w-[378px] lg:max-w-[378px] px-4 lg:mx-auto">
+        <div className="flex my-4 items-center">
+          <img src={mobileLogin} className="w-[200px]" />
+        </div>
+        <p className="text-[#0e001a] mb-2 text-[1.3rem] font-semibold leading-[1.8rem] ">
+          Let's get started!
+        </p>
+        <form onSubmit={submitHandler} className="flex flex-col w-full">
           <div className="flex flex-col mb-4">
-            <div className="inputCont">
-              <label className="inputlabel">First Name</label>
-              <input
-                placeholder="First Name"
-                className="input"
-                onChange={(e) => setFirstName(e.target.value)}
-                required
-              />
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-col w-[48%]">
+                <label className="inputlabel">First Name</label>
+                <input
+                  placeholder="First Name"
+                  className="input"
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="flex flex-col w-[48%]">
+                <label className="inputlabel">Last Name</label>
+                <input
+                  placeholder="Last Name"
+                  className="input"
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                />
+              </div>
             </div>
-            <div className="inputCont">
-              <label className="inputlabel">Last Name</label>
-              <input
-                placeholder="Last Name"
-                className="input"
-                onChange={(e) => setLastName(e.target.value)}
-                required
-              />
-            </div>
+
             <div className="inputCont">
               <label className="inputlabel">E-mail</label>
               <input
@@ -120,46 +128,37 @@ export default function SignUp() {
                 required
               />
             </div>
-            <div className="inputCont">
-              <label className="inputlabel">Confirm Password</label>
-              <input
-                className="input"
-                type="password"
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
-            </div>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col ">
             {loading ? (
               <button
-                className="bg-slate-300 text-white py-2 px-6 rounded flex justify-center items-center"
+                className="bg-slate-300 text-white  p-4 rounded flex justify-center items-center "
                 type="submit"
               >
                 <CircularProgress size={25} thickness={4} color="inherit" />
               </button>
             ) : (
               <button
-                className="bg-teal-500 text-white py-2 px-6 rounded"
+                className="bg-[#0e001a] text-white p-4 rounded-lg hover:bg-[#0e001ab7] transition"
                 type="submit"
               >
-                Sign Up
+                Sign up
               </button>
             )}
           </div>
-          <div className="flex mt-2">
+          <div className="flex mt-4 text-sm font-normal leading-4 tracking-[0]">
             Already have an account?
             <Link
               className="text-sky-500 ml-2"
               to={`/signin?redirect=${redirect}`}
             >
-              Sign In.
+              Sign in.
             </Link>
           </div>
         </form>
-        <div className="md:flex hidden flex-col items-center">
-          <img src={mobileLogin} className="w-[500px]" />
-        </div>
+        <p className="text-xs font-normal leading-4 tracking-[0] text-center w-[300px] text-[#7f8286] mt-6">
+          By continuing, you agree to our Privacy Policy and Terms of Use.
+        </p>
       </div>
     </div>
   );
